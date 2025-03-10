284b29b112cee1198882d4971ca35b54eefe3494
V1p3r <juanfra.riera2003@gmail.com>
2025-03-10 12:01:08 +0100

    Add files via upload

---
+++ IA_P2-main/__main__.py
@@ -0,0 +1,31 @@
+import logging
+
+from reinforcement.agent import AgentQ
+from reinforcement.agentSARSA import AgentSARSA
+from reinforcement.joc import Laberint
+
+
+def main():
+    logging.basicConfig(
+        format="%(levelname)-8s: %(asctime)s: %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        level=logging.INFO,
+    )  # Only show messages *equal to or above* this level
+
+    game = Laberint()
+    agent = AgentSARSA(game)
+    h, w, _ = agent.train(
+        discount=0.90,
+        exploration_rate=0.10,
+        learning_rate=0.6,
+        episodes=10000,
+        stop_at_convergence=True,
+    )
+
+    game.reset()
+    game.set_agent([agent])
+    game.comencar()
+
+
+if __name__ == "__main__":
+    main()
